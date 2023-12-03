193ba0b4bd7f1f99bc7b98eed83c604b67aa4c9b
nazrinsultanli <nazrindolkhanova@gmail.com>
2023-12-03 22:34:09 +0400

    testPr

--- TheMovieApp/Controller/HomePageViewController.swift
+++ TheMovieApp/Controller/HomePageViewController.swift
@@ -8,13 +8,56 @@
 import UIKit
 
 class HomePageViewController: UIViewController {
-var viewModel = HomePageViewModel()
+    var viewModel = HomePageViewModel()
+    
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.delegate = self
+        table.dataSource = self
+        table.backgroundColor = .green
+        table.register(HomePageTableViewCell.self, forCellReuseIdentifier: HomePageTableViewCell.reuseID)
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Movie"
+        view.backgroundColor = .red
+        
+        setUpTable()
         viewModel.getPopularMovie()
         viewModel.success = { [weak self] in
-            print("good")
+            self?.tableView.reloadData()
         }
     }
+    
+    private func setUpTable() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: 0)])
+        
+    }
+}
 
+
+extension HomePageViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.popularMovies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: HomePageTableViewCell.reuseID, for: indexPath) as! HomePageTableViewCell
+        cell.configure(item: viewModel.popularMovies[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        100
+    }
 }
---
+++ TheMovieApp/View/HomePageTableViewCell.swift
@@ -0,0 +1,97 @@
+//
+//  HomePageTableViewCell.swift
+//  TheMovieApp
+//
+//  Created by Nazrin Sultanlı on 03.12.23.
+//
+
+import UIKit
+
+class HomePageTableViewCell: UITableViewCell {
+    
+    static let reuseID = "HomePageTableViewCell"
+    
+    private let titleLabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 10)
+        return label
+    }()
+    
+    private let descriptionLabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    private let priceLabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    private let myImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.layer.cornerRadius = 10
+        image.layer.masksToBounds = true
+        return image
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpCell()
+    }
+
+    
+    required init?(coder: NSCoder) {
+        fatalError("Init(coder:) has not been implemented")
+    }
+    
+    
+    private func setUpCell() {
+        let stack = UIStackView()
+        stack.addArrangedSubview(titleLabel)
+        stack.addArrangedSubview(descriptionLabel)
+        stack.addArrangedSubview(priceLabel)
+        stack.axis = .vertical
+        stack.spacing = 4
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(stack)
+        contentView.addSubview(myImage)
+        
+        NSLayoutConstraint.activate([
+            stack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            stack.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+        
+            myImage.widthAnchor.constraint(equalToConstant: 110),
+            myImage.heightAnchor.constraint(equalToConstant: 80),
+            myImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
+            myImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+    
+    func configure(item: Result) {
+        titleLabel.text = item.title
+        descriptionLabel.text = item.overview
+        priceLabel.text = item.originalTitle
+        
+        
+        if let thumbnailUrl = URL(string: item.backdropPath ?? "") {
+                // Load image from URL using URLSession
+                URLSession.shared.dataTask(with: thumbnailUrl) { (data, _, error) in
+                    if let error = error {
+                        print("Error loading image: \(error.localizedDescription)")
+                    } else if let data = data {
+                        DispatchQueue.main.async {
+                            self.myImage.image = UIImage(data: data)
+                        }
+                    }
+                }.resume()
+            }
+
+    }
+}
