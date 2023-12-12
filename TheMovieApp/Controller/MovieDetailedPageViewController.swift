eb17bc1e94a069f2afa0048f8a59436e6e22a5ce
nazrinsultanli <nazrindolkhanova@gmail.com>
2023-12-13 02:15:13 +0400

    movieGestureError

--- TheMovieApp/Controller/MovieDetailedPageViewController.swift
+++ TheMovieApp/Controller/MovieDetailedPageViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class MovieDetailedPageViewController: UIViewController {
     var selectedMovie: MovieResult?
+    var genres = [String]()
+    let genreArray = GenresHelper.shared.genres
+    private let scrollViewm: UIScrollView = {
+        let sv = UIScrollView()
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        return sv
+    }()
+    
+    private let contentViewm: UIView = {
+        let sv = UIView()
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        return sv
+    }()
     
     private lazy var movieImage: UIImageView = {
         let imageViewm = UIImageView()
@@ -83,47 +96,50 @@ class MovieDetailedPageViewController: UIViewController {
         return imageViewm
     }()
     
-    private lazy var genreLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.boldSystemFont(ofSize: 12)
-        label.textAlignment = .left
-        label.numberOfLines = 0
-        return label
+    private lazy var containerGenreView: UIView = {
+            let view = UIView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            // Additional setup for your container view
+            
+            return view
+        }()
+        
+    private lazy var genresInfoView: LabelAndCollectionView = {
+        let view = LabelAndCollectionView(labelText: "Genres")
+        return view
     }()
     
-//    private lazy var scrollView: UIScrollView = {
-//        
-//    }
-//    private lazy var genresCollectionView: UICollectionView = {
-//        let layout = UICollectionViewFlowLayout()
-//        layout.scrollDirection = .horizontal
-//        
-//        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
-//        collection.showsHorizontalScrollIndicator = false
-//        collection.translatesAutoresizingMaskIntoConstraints = false
-//        collection.dataSource = self
-//        collection.delegate = self
-//        collection.register(HorizontalLabelsCollectionCell.self, forCellWithReuseIdentifier: HorizontalLabelsCollectionCell.reuseID)
-//        return collection
-//    }()
+    private lazy var directorsInfoView: LabelAndCollectionView = {
+            let view = LabelAndCollectionView(labelText: "Directors")
+            return view
+        }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        createSegmentedControl()
+        configure()
         setUpConstraints()
+        
+    }
 
+    func configure() {
+        movieImage.loadImage(url: selectedMovie?.posterPath ?? "")
+        titleLabel.text = selectedMovie?.title
+        languageLabel.text = selectedMovie?.originalLanguage ?? ""
+        durationLabel.text = selectedMovie?.releaseDate ?? ""
+        ratingLabel.text = "\(String(describing: selectedMovie?.voteAverage))/10"
+        
+        if let genreIDs = selectedMovie?.genreIDS {
+                for genreIndex in genreIDs {
+                    if genreIndex >= 0, genreIndex < genreArray.count {
+                        genres.append(genreArray[genreIndex].name ?? "" )
+                    }
+                }
+            }
+        genresInfoView.data = genres
+        directorsInfoView.data = ["asas","frfrf","ioio"]
+        
     }
-    func createSegmentedControl() {
-        let items = ["Details", "Trails","Cast","Shots"]
-        let segmentedControl = UISegmentedControl(items: items)
-        segmentedControl.addTarget(self, action: #selector(suitDidChange(_:)), for: .valueChanged)
-        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        segmentedControl.tintColor = .blue
-        view.addSubview(segmentedControl)
-    }
-    
     @objc func suitDidChange(_ segmentedControl: UISegmentedControl) {
         switch segmentedControl.selectedSegmentIndex {
         case 0:
@@ -139,52 +155,89 @@ class MovieDetailedPageViewController: UIViewController {
         }
     }
     func setUpConstraints() {
-        view.addSubview(movieImage)
-        view.addSubview(titleLabel)
+        view.addSubview(scrollViewm)
+        scrollViewm.addSubview(contentViewm)
+        
+        let hConst = contentViewm.heightAnchor.constraint(equalTo: scrollViewm.heightAnchor)
+        hConst.isActive = true
+        hConst.priority = UILayoutPriority(50)
+        contentViewm.addSubview(movieImage)
+        contentViewm.addSubview(titleLabel)
         
         
         let stackLanguage = UIStackView()
         stackLanguage.addArrangedSubview(languageLabel)
         stackLanguage.addArrangedSubview(languageImage)
+        stackLanguage.axis = .horizontal
+        stackLanguage.translatesAutoresizingMaskIntoConstraints = false
         
         let stackDuration = UIStackView()
         stackDuration.addArrangedSubview(durationLabel)
         stackDuration.addArrangedSubview(durationImage)
+        stackDuration.axis = .horizontal
+        stackDuration.translatesAutoresizingMaskIntoConstraints = false
         
         let stackRating = UIStackView()
-        stackDuration.addArrangedSubview(ratingLabel)
+        stackRating.addArrangedSubview(ratingLabel)
         stackRating.addArrangedSubview(ratingImage)
+        stackRating.axis = .horizontal
+        stackRating.translatesAutoresizingMaskIntoConstraints = false
         
         let stackLanDurRa = UIStackView()
         stackLanDurRa.addArrangedSubview(stackLanguage)
         stackLanDurRa.addArrangedSubview(stackDuration)
         stackLanDurRa.addArrangedSubview(stackRating)
         stackLanDurRa.spacing = 6
+        stackLanDurRa.axis = .horizontal
         stackLanDurRa.translatesAutoresizingMaskIntoConstraints = false
         
+        contentViewm.addSubview(stackLanDurRa)
+        
+        let items = ["Details", "Trails","Cast","Shots"]
+        let segmentedControl = UISegmentedControl(items: items)
+        segmentedControl.addTarget(self, action: #selector(suitDidChange(_:)), for: .valueChanged)
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        segmentedControl.tintColor = .blue
+        contentViewm.addSubview(segmentedControl)
+        contentViewm.addSubview(genresInfoView)
+        contentViewm.addSubview(directorsInfoView)
+        
         NSLayoutConstraint.activate([
-            movieImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
-            movieImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            movieImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            movieImage.heightAnchor.constraint(equalToConstant: 200),
+            scrollViewm.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollViewm.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollViewm.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollViewm.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            contentViewm.topAnchor.constraint(equalTo: scrollViewm.topAnchor),
+            contentViewm.bottomAnchor.constraint(equalTo: scrollViewm.bottomAnchor),
+            contentViewm.leadingAnchor.constraint(equalTo: scrollViewm.leadingAnchor),
+            contentViewm.trailingAnchor.constraint(equalTo: scrollViewm.trailingAnchor),
+            contentViewm.widthAnchor.constraint(equalTo: scrollViewm.widthAnchor),
+            
+            movieImage.topAnchor.constraint(equalTo: contentViewm.topAnchor, constant: 0),
+            movieImage.leadingAnchor.constraint(equalTo: contentViewm.leadingAnchor, constant: 20),
+            movieImage.trailingAnchor.constraint(equalTo: contentViewm.trailingAnchor, constant: -20),
+            movieImage.heightAnchor.constraint(equalToConstant: 300),
             
             titleLabel.topAnchor.constraint(equalTo: movieImage.bottomAnchor, constant: 20),
-            movieImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            movieImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            movieImage.heightAnchor.constraint(equalToConstant: 30),
+            titleLabel.leadingAnchor.constraint(equalTo: contentViewm.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: contentViewm.trailingAnchor, constant: -20),
+            titleLabel.heightAnchor.constraint(equalToConstant: 30),
             
             stackLanDurRa.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
-            stackLanDurRa.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            stackLanDurRa.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            stackLanDurRa.leadingAnchor.constraint(equalTo: contentViewm.leadingAnchor, constant: 20),
+            stackLanDurRa.trailingAnchor.constraint(equalTo: contentViewm.trailingAnchor, constant: -20),
             stackLanDurRa.heightAnchor.constraint(equalToConstant: 30),
             
-            stackLanDurRa.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
-            stackLanDurRa.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            stackLanDurRa.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            stackLanDurRa.heightAnchor.constraint(equalToConstant: 30)
+            segmentedControl.topAnchor.constraint(equalTo: stackLanDurRa.bottomAnchor, constant: 20),
+            segmentedControl.leadingAnchor.constraint(equalTo: contentViewm.leadingAnchor, constant: 20),
+            segmentedControl.trailingAnchor.constraint(equalTo: contentViewm.trailingAnchor, constant: -20),
+            segmentedControl.heightAnchor.constraint(equalToConstant: 50)
             
             
-        
+            
+            
+            
         ])
     }
     
